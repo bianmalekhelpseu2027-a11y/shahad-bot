b6cfc13b3d3b6e520989372ae37768c51379915d
bianmalekhelpseu2027-a11y <bianmalekhelpseu2027@gmail.com>
2026-05-29 23:28:35 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,53 +1,41 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-import asyncio
 
-BOT_TOKEN = "8819650642:AAGg5vxB7CMDFnmaDE7vCDMDnqxba4MFHOQ"
 
-# تهيئة البوت
+BOT_TOKEN = "8819650642:AAFZBmLZUdStO4CUJ4I-6f1xNhCI3KUEFG0"
+
 app = Client("shahad_bot", api_id=2040, api_hash="b18441a1ff607e10a989891a5462e627", bot_token=BOT_TOKEN)
+
+# قاموس لتسهيل إدارة الكلمات والروابط
+replies = {
+    "آيلتس": ("اختبار آيلتس (IELTS):", "https://t.me/httpsmO9QD5Mbb_FhYzRk/30"),
+    "تواصل": ("التواصل:", "https://t.me/httpsmO9QD5Mbb_FhYzRk/3"),
+    "موعد": ("موعد التقديم:", "https://t.me/httpsmO9QD5Mbb_FhYzRk/17"),
+    "تقديم": ("التقديم:", "https://t.me/httpsmO9QD5Mbb_FhYzRk/18"),
+    "مستندات": ("المستندات المطلوبة:", "https://t.me/httpsmO9QD5Mbb_FhYzRk/20"),
+    "درجات": ("الدرجات:", "https://t.me/httpsmO9QD5Mbb_FhYzRk/21"),
+    "خطط": ("الخطط:", "https://t.me/httpsmO9QD5Mbb_FhYzRk/22"),
+    "جدول": ("الجدول:", "https://t.me/httpsmO9QD5Mbb_FhYzRk/23"),
+    "فروع": ("فروع الجامعة:", "https://t.me/httpsmO9QD5Mbb_FhYzRk/24"),
+    "إعادة": ("إعادة قيد:", "https://t.me/httpsmO9QD5Mbb_FhYzRk/25"),
+    "انسحاب": ("الانسحاب:", "https://t.me/httpsmO9QD5Mbb_FhYzRk/26"),
+    "تأجيل": ("التأجيل:", "https://t.me/httpsmO9QD5Mbb_FhYzRk/27"),
+    "دمج": ("الدمج:", "https://t.me/httpsmO9QD5Mbb_FhYzRk/28"),
+    "تسريع": ("التسريع الأكاديمي:", "https://t.me/httpsmO9QD5Mbb_FhYzRk/29"),
+    "ستيب": ("اختبار ستيب (STEP):", "https://t.me/httpsmO9QD5Mbb_FhYzRk/4"),
+    "خدمات": ("الخدمات:", "https://t.me/httpsmO9QD5Mbb_FhYzRk/16"),
+    "بنك": ("بنك ستيب:", "https://t.me/httpsmO9QD5Mbb_FhYzRk/6"),
+    "تجميعات": ("التجميعات والملخصات:", "https://t.me/httpsmO9QD5Mbb_FhYzRk/31"),
+    "ملخصات": ("التجميعات والملخصات:", "https://t.me/httpsmO9QD5Mbb_FhYzRk/31"),
+    "ارشيف": ("أرشيف التحضيري:", "https://t.me/httpsmO9QD5Mbb_FhYzRk")
+}
 
 @app.on_message(filters.text & ~filters.private)
 async def smart_reply(client, message):
     text = message.text
-    
-    if "آيلتس" in text:
-        await message.reply("اختبار آيلتس (IELTS):", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("رابط الآيلتس", url="https://t.me/httpsmO9QD5Mbb_FhYzRk/30")]]))
-    elif "تواصل" in text:
-        await message.reply("التواصل:", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("رابط التواصل", url="https://t.me/httpsmO9QD5Mbb_FhYzRk/3")]]))
-    elif "موعد" in text:
-        await message.reply("موعد التقديم:", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("رابط الموعد", url="https://t.me/httpsmO9QD5Mbb_FhYzRk/17")]]))
-    elif "تقديم" in text:
-        await message.reply("التقديم:", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("رابط التقديم", url="https://t.me/httpsmO9QD5Mbb_FhYzRk/18")]]))
-    elif "مستندات" in text:
-        await message.reply("المستندات المطلوبة:", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("رابط المستندات", url="https://t.me/httpsmO9QD5Mbb_FhYzRk/20")]]))
-    elif "درجات" in text:
-        await message.reply("الدرجات:", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("رابط الدرجات", url="https://t.me/httpsmO9QD5Mbb_FhYzRk/21")]]))
-    elif "خطط" in text:
-        await message.reply("الخطط:", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("رابط الخطط", url="https://t.me/httpsmO9QD5Mbb_FhYzRk/22")]]))
-    elif "جدول" in text:
-        await message.reply("الجدول:", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("رابط الجدول", url="https://t.me/httpsmO9QD5Mbb_FhYzRk/23")]]))
-    elif "فروع" in text:
-        await message.reply("فروع الجامعة:", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("رابط الفروع", url="https://t.me/httpsmO9QD5Mbb_FhYzRk/24")]]))
-    elif "إعادة" in text:
-        await message.reply("إعادة قيد:", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("رابط إعادة القيد", url="https://t.me/httpsmO9QD5Mbb_FhYzRk/25")]]))
-    elif "انسحاب" in text:
-        await message.reply("الانسحاب:", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("رابط الانسحاب", url="https://t.me/httpsmO9QD5Mbb_FhYzRk/26")]]))
-    elif "تأجيل" in text:
-        await message.reply("التأجيل:", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("رابط التأجيل", url="https://t.me/httpsmO9QD5Mbb_FhYzRk/27")]]))
-    elif "دمج" in text:
-        await message.reply("الدمج:", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("رابط الدمج", url="https://t.me/httpsmO9QD5Mbb_FhYzRk/28")]]))
-    elif "تسريع" in text:
-        await message.reply("التسريع الأكاديمي:", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("رابط التسريع", url="https://t.me/httpsmO9QD5Mbb_FhYzRk/29")]]))
-    elif "ستيب" in text:
-        await message.reply("اختبار ستيب (STEP):", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("رابط ستيب", url="https://t.me/httpsmO9QD5Mbb_FhYzRk/4")]]))
-    elif "خدمات" in text:
-        await message.reply("الخدمات:", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("رابط الخدمات", url="https://t.me/httpsmO9QD5Mbb_FhYzRk/16")]]))
-    elif "بنك" in text:
-        await message.reply("بنك ستيب:", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("رابط بنك ستيب", url="https://t.me/httpsmO9QD5Mbb_FhYzRk/6")]]))
-    elif "تجميعات" in text or "ملخصات" in text:
-        await message.reply("التجميعات والملخصات:", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("رابط التجميعات", url="https://t.me/httpsmO9QD5Mbb_FhYzRk/31")]]))
-    elif "ارشيف" in text:
-        await message.reply("أرشيف التحضيري:", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("رابط الأرشيف", url="https://t.me/httpsmO9QD5Mbb_FhYzRk")]]))
+    for keyword, (caption, url) in replies.items():
+        if keyword in text:
+            await message.reply(caption, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("رابط المعلومة", url=url)]]))
+            break
 
 app.run()
